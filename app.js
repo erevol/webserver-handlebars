08854dff68a81c6e6b7d0e9cd8a7521a431a683c
Eugenia Revol <euge.revol@gmail.com>
2021-04-22 21:27:47 -0300

    Webserver initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const port = process.env.PORT;
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials', (err) => {});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('home', { title: 'The title', id: 'HG123'}));
+app.get('/generic', (req, res) => res.render('generic'));
+app.get('/elements', (req, res) => res.render('elements'));
+
+// app.get('/index', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+// app.get('/generic', (req, res) => res.sendFile(`${__dirname}/public/generic.html`));
+// app.get('/elements', (req, res) => res.sendFile(`${__dirname}/public/elements.html`));
+
+app.get('*', (req, res) => res.sendFile(`${__dirname}/public/404.html`));
+ 
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+});
+  
